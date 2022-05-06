3e0ebd58443368605e59bd07dadeb2764a94fdac
ShengTao <taosheng@corp-ci.com>
2022-05-06 17:51:45 +0800

    feat(*): relay fix

--- app/v1/access/model/mmysql/relay.go
+++ app/v1/access/model/mmysql/relay.go
@@ -6,13 +6,14 @@ import (
 
 type Relay struct {
 	gorm.Model
-	Name    string `json:"name"`
-	UUID    string `json:"uuid" gorm:"column:uuid"`
-	Host    string `json:"host"`
-	Port    int    `json:"port"`
-	OutPort int    `json:"out_port"`
-	CaPem   string `json:"ca_pem"`
-	CertPem string `json:"cert_pem"`
+	UserUUID string `json:"user_uuid" gorm:"user_uuid"`
+	Name     string `json:"name"`
+	UUID     string `json:"uuid" gorm:"column:uuid"`
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	OutPort  int    `json:"out_port"`
+	CaPem    string `json:"ca_pem"`
+	CertPem  string `json:"cert_pem"`
 }
 
 func (Relay) TableName() string {
